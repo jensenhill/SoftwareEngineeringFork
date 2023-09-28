26c604d88c2e0aedb1cb24bfbf5e225f8dbbda7c
jensenhill <91635059+jensenhill@users.noreply.github.com>
2023-09-28 15:29:40 +0100

    Task completed.

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -7,9 +7,11 @@ int main()
 
 
     // ************************ Using printf ******************************* 
-    printf("Hello Everyone. We are using C\nHow are things?\n");
+    printf("Hello Everyone. \nWe are using C\nHow are things?");
     // TASK (a) - remove \n from the string above - what does this change? 
-    
+
+        // This leaves the text on the same line at output.
+
     // ******************* Using printf placeholders *********************** 
     int age = 23;
     printf("My age is %d\n", age);
@@ -21,7 +23,7 @@ int main()
     // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
     // Write solution here
     
-    //
+    printf("Area = %.3f\n", area);
 
     // You can output complex strings to the terminal
     int p = 3;
@@ -31,7 +33,7 @@ int main()
     // TASK(c) - using printf, display the radius and the area using a single printf statement
     // Write solution here
 
-    //
+    printf("Radius is %.3f, Area is %.3f.", radius, area);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
