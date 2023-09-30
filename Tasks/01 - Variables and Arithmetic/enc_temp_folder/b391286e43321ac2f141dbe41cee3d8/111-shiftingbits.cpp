7009ac045a26a94f864e5557b24953816cc3ad11
jensenhill <91635059+jensenhill@users.noreply.github.com>
2023-09-30 15:06:02 +0100

    Task complete.

--- Tasks/01 - Variables and Arithmetic/enc_temp_folder/b391286e43321ac2f141dbe41cee3d8/111-shiftingbits.cpp
+++ Tasks/01 - Variables and Arithmetic/enc_temp_folder/b391286e43321ac2f141dbe41cee3d8/111-shiftingbits.cpp
@@ -29,10 +29,8 @@ int main()
 	//TASKS:
 
 	//Shift right by 1 and display
-	number >>= 1;
-	displayAndCompare(number);
+
 
 	//Shift left by 1 and display
-	number <<= 1;
-	displayAndCompare(number);
+
 }
